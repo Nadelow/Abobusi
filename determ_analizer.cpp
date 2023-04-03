984a76e45952000eedfcc176c3ec84819ee68667
NekoChanFan <106037729+NekoChanFan@users.noreply.github.com>
2023-04-03 23:54:33 +0300

    Updated determ_analizer.h & .cpp
    Added discriptions and fixed encoding issue. Also moved some code from .h to .cpp file for more clarity. Renamed determ_analizer class to Determ_analyzer

--- determ_analizer.cpp
+++ determ_analizer.cpp
@@ -1,16 +1,35 @@
 #include "determ_analizer.h"
 
-//determ_analizer::determ_analizer(std::string filename)
-//{
-//	m_input_file = std::fstream(filename);
-//	if (!m_input_file)
-//	{
-//		std::cout << "In determ_analizer(std::string filename) Couldn't open a file " << filename << std::endl;
-//		return;
-//	}
-//
-//	fill_states();
-//	fill_sym_lexems();
-//	fill_transition_table();
-//}
+Symbolic_token& Symbolic_token::operator=(const Symbolic_token& obj)
+{
+    m_s_lex_name = obj.m_s_lex_name; 
+    m_s_lex_id = obj.m_s_lex_id; 
+    m_s_lex_value = obj.m_s_lex_value; 
+    return *this;
+}
 
+State& State::operator=(const State& obj)
+{
+    m_name = obj.m_name; 
+    m_id = obj.m_id; 
+
+    return *this;
+}
+
+int operator<(const State& obj1, const State& obj2)
+{
+    return obj1.m_id < obj2.m_id;
+}
+
+int operator<(const Lexem& obj1, const Lexem& obj2)
+{
+    return obj1.m_lex_id < obj2.m_lex_id;
+}
+
+Lexem& Lexem::operator=(const Lexem& obj)
+{
+    m_lex_class = obj.m_lex_class; 
+    m_lex_id = obj.m_lex_id; 
+
+    return *this;
+}
--- determ_analizer.h
+++ determ_analizer.h
@@ -7,52 +7,73 @@
 
 class State {
 public:
-	std::string m_name;
-	size_t m_id;
+	std::string m_name;                //Имя состояния 
+	size_t m_id;                       //Идентификатор состояния(для каждого имени уникальный)
+
+	//Конструктор с параметрами по умолчанию. name_ - имя состояния, id_ - идентификатор
 	State(const std::string& name_ = "", size_t id_ = 0) : m_name(name_),  m_id(id_){}
-	int friend operator<(const State& obj1, const State& obj2) { return obj1.m_id < obj2.m_id; }
-	State& operator=(const State& obj) { m_name = obj.m_name; m_id = obj.m_id; return *this; }
+
+	//Оператор <. Сравниваются только id
+	int friend operator<(const State& obj1, const State& obj2);
+
+	//Оператор присваивания
+	State& operator=(const State& obj);
 };
 
 class Symbolic_token {
 public:
-	std::string m_s_lex_name;          //��� �������
-	int m_s_lex_value;                 //�������� ���������� �������
-	size_t m_s_lex_id;                 //id �������
+	std::string m_s_lex_name;          //имя лексемы
+	int m_s_lex_value;                 //значение символьной лексемы
+	size_t m_s_lex_id;                 //id лексемы
+
+	//Конструктор с параметрами по умолчанию. name_ - имя, value_ - значение, id_ - идентификатор
 	Symbolic_token(std::string name_ = "", int value_ = -1, size_t id_ = 0) : m_s_lex_name(name_), m_s_lex_value(value_), m_s_lex_id(id_){};
-	Symbolic_token& operator=(const Symbolic_token& obj) { m_s_lex_name = obj.m_s_lex_name; m_s_lex_id = obj.m_s_lex_id; m_s_lex_value = obj.m_s_lex_value; return *this; };
+
+	//Оператор присваивания
+	Symbolic_token& operator=(const Symbolic_token& obj);
+
+	//Оператор <. Сравниваются только id
 	int friend operator<(const Symbolic_token& obj1_, const Symbolic_token& obj2_) { return obj1_.m_s_lex_id < obj2_.m_s_lex_id; }
 };
 
 class Lexem {
 public:
-	size_t m_lex_id;                  //id �������
-	std::string m_lex_class;          //����� �������
+	size_t m_lex_id;                  //id лексемы
+	std::string m_lex_class;          //класс лексемы
+
+	//Конструктор с параметрами по умолчанию. lex_class - класс лексемы(std::string), id - идентификатор(уникальный для каждого класса)
 	Lexem(std::string lex_class = "", size_t id = 0) : m_lex_class(lex_class), m_lex_id(id) {}
-	Lexem& operator=(const Lexem& obj) { m_lex_class = obj.m_lex_class; m_lex_id = obj.m_lex_id; return *this; }
-	int friend operator<(const Lexem& obj1, const Lexem& obj2) { return obj1.m_lex_id < obj2.m_lex_id; }
+
+	//Оператор присваивания
+	Lexem& operator=(const Lexem& obj);
+	
+	//Оператор <. Сравниваются только id
+	int friend operator<(const Lexem& obj1, const Lexem& obj2);
 };
 
-//���������� ��������� �� �������. ������������ �������� State - ���������, � ������� ������� ��������� ����� ���������� ���������
-typedef State(*funct_ptr)();
+//объявление указателя на функции. Возвращаемое значение State - состояние, в которое автомат переходит после выполнения процедуры
+typedef State(Determ_analizer::*det_func_ptr)();
 
 
-class determ_analizer
+class Determ_analizer
 {
 protected:
-	std::map<State, std::map<Symbolic_token, funct_ptr>> m_transition_table; //������� ��������� �����������
-	State m_cur_state; //������� ���������
-	Symbolic_token m_cur_sym; //������� ������
-	std::fstream m_input_file; //����
-	std::map<std::string, Symbolic_token> m_sym_lexems; //��������� ���������� ������
-	std::map<std::string, State> m_states;
+	std::map<State, std::map<Symbolic_token, det_func_ptr>> m_transition_table; //Таблица переходов анализатора
+	State m_cur_state;                                                          //Текущее состояние
+	Symbolic_token m_cur_sym;                                                   //Текущий символ
+	std::fstream m_input_file;                                                  //Файл
+	std::map<std::string, Symbolic_token> m_sym_lexems;                         //Коллекция символьных лексем
+	std::map<std::string, State> m_states;                                      //Коллекция состояний
 
-	virtual Symbolic_token transliterator(int sym) = 0; //����������� ������� ��������������. �� �������� ������� ���������� ����� ������� � ����������� �� ��������.
-public:
+	//Виртуальная функция транслитерации. По входному символу определяет класс лексемы и присваивает ей значение.
+	virtual Symbolic_token transliterator(int sym) = 0; 
+	//Виртуальная функция заполнения коллекции символьных лексем.
 	virtual void fill_sym_lexems() = 0;
+	//Виртуальная функция заполнения коллекции состояний.
 	virtual void fill_states() = 0;
+	//Виртуальная функция заполнения таблицы переходов.
 	virtual void fill_transition_table() = 0;
-	//determ_analizer(std::string filename); //����������� ��������� �����������
-	virtual void parse() = 0;  //������� ��������
+public:
+	virtual void parse() = 0;  //Функция парсинга
 };
 
